f15f0f38c949949884b4ee53406b751aa1b20453
Lihao Deng <lihao-deng@outlook.com>
2022-04-09 21:15:34 +0800

    1

--- folder/access_picUrl.py
+++ folder/access_picUrl.py
@@ -38,7 +38,7 @@ def access_picUrl():
                     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36 Edg/100.0.1185.29"
                 }
 
-                response = requests.get(url, headers=headers).text
+                response = requests.get(url, headers=headers, timeout=3).text
 
                 dict_response = json.loads(response)
 
